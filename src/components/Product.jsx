4462ad97c0be957cfc0529a67fc05dd752600246
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-01-23 18:05:36 -0800

    Updated display price

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,8 @@
 export default function Product( {productName, description, price, quantity, addToCart} ) {
+    let displayPrice = (price).toFixed(2);
     return (
         <div className="product-container">
-            <p>{productName}: <span className="product-price">${price}</span></p>
+            <p>{productName}: <span className="product-price">${displayPrice}</span></p>
             <p>{description}</p>
             <button onClick={ () => { 
                 addToCart(
@@ -10,8 +11,8 @@ export default function Product( {productName, description, price, quantity, add
                         price: price,
                         quantity: quantity
                     }
-                    )}} className="add-product-btn" >
-                Add
+                    )}} className="add-product-btn" type="button" >
+                Add to Cart
             </button>
         </div>
     )
